519e5cdfef91a133aa26351cd9f23c092a593369
Abhishek Atole <85939904+Abhishek-Atole@users.noreply.github.com>
2025-05-22 12:58:02 +0000

    Doc:Reume Update

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -1,4 +1,3 @@
-
 import { Download, FileText, Briefcase, Calendar, MapPin, GitBranch, Database, FileCode, Network, Computer } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
